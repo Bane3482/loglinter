64fe4311fbb9fec046062fce02c1168501d3fc62
John Doe <johndoe@example.com>
2026-03-15 23:45:16 +0300

    new changes

--- cmd/loglinter/main.go
+++ cmd/loglinter/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"golang.org/x/tools/go/analysis/singlechecker"
 
-	"github.com/Bane3482/go-log-message-linter/pkg/analyzer"
+	"github.com/Bane3482/go-log-message-linter/pkg/loglinter"
 )
 
 func main() {
-	singlechecker.Main(analyzer.Analyzer)
+	singlechecker.Main(loglinter.Analyzer)
 }
