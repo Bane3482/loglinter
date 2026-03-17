915b720c819e86e73aa03e488f4f412747d5ccff
John Doe <johndoe@example.com>
2026-03-17 10:40:54 +0300

    new message

---
+++ plugin/plugin.go
@@ -0,0 +1,25 @@
+package loglinter
+
+import (
+	"github.com/Bane3482/loglinter/pkg/loglinter"
+	"github.com/golangci/plugin-module-register/register"
+	"golang.org/x/tools/go/analysis"
+)
+
+func init() {
+	register.Plugin("loglinter", New)
+}
+
+type plugin struct{}
+
+func New(settings any) (register.LinterPlugin, error) {
+	return &plugin{}, nil
+}
+
+func (plugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
+	return []*analysis.Analyzer{loglinter.Analyzer}, nil
+}
+
+func (plugin) GetLoadMode() string {
+	return register.LoadModeSyntax
+}
